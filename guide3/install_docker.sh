ee23ccecf8c60ecea3e0eaeb009467b772e389b6
Persa053 <pg50219@alunos.uminho.pt>
2023-10-19 15:29:49 +0100

    script pra instalar docker na VM e permitir executar como non-root user

---
+++ guide3/install_docker.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+set +e
+# Uninstall all conflicting packages
+echo Uninstall all conflicting packages
+echo ----------------------------------
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+# Add Docker's official GPG key:
+echo Add docker GPG key
+echo ----------------------------------
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg -y
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg 
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo Add the repository to Apt sources
+echo ----------------------------------
+echo   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Install the latest version
+echo Install the latest version
+echo ----------------------------------
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+# Manage Docker as a non-root user
+echo Manage Docker as a non-root user
+echo ----------------------------------
+sudo usermod -aG docker $USER
+newgrp docker
